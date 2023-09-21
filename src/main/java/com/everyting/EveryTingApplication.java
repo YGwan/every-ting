1d8ee4dccb999e6a9584f9a949afdbc4b9ac9a4f
good0619@naver.com <32181771@dankook.ac.kr>
2023-09-21 13:04:32 +0900

    set env

---
+++ src/main/java/com/everyting/EveryTingApplication.java
@@ -0,0 +1,13 @@
+package com.everyting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryTingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryTingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/everyting/EveryTingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everyting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryTingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

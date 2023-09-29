29f883153dad4f792684a13c4d04dbb1c0ccfb4a
sol <83967710+solpinetree@users.noreply.github.com>
2023-09-29 23:10:10 +0900

    feat: team 모듈 - scanBasePackages 설정 (#9)
    core의 GlobalControllerAdvice를 사용하기 위해 core 모듈도 base package로 인식하도록 설정합니다.

--- module-team/src/main/java/com/everyTing/team/TeamApplication.java
+++ module-team/src/main/java/com/everyTing/team/TeamApplication.java
@@ -3,7 +3,9 @@ package com.everyTing.team;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {"com.everyTing.core", "com.everyTing.team"}
+)
 public class TeamApplication {
 
     public static void main(String[] args) {

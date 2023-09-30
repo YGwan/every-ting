52eacbf2153ac59a52f04609b4be09f48bec8fe8
sol <sadlthf@icloud.com>
2023-10-01 05:18:00 +0900

    chore : 팀 해시태그 더미데이터 추가 (#12)

--- module-team/src/main/resources/data.sql
+++ module-team/src/main/resources/data.sql
@@ -1,5 +1,22 @@
 -- 과팅 더미 데이터
-insert into team (id, name, region, university, major, code, member_limit, gender, created_at, updated_at)
-values (
-        1, "여기여기모여라", "경기 남부", "단국대학교", "응용 컴퓨터 공학과", "Abeaegs3bcq!", 3, "FEMALE", now(), now()
-       );
+insert into team (id, name, region, university, major, code, member_limit, gender, created_at,
+                  updated_at)
+values (1,
+        "여기여기모여라",
+        "경기 남부",
+        "단국대학교",
+        "응용 컴퓨터 공학과",
+        "Abeaegs3bcq!",
+        3,
+        "FEMALE", now(), now());
+
+-- 팀 해시태그 더미 데이터
+insert into team_hashtag (id, team_id, content, created_at)
+values (1,
+        1,
+        "모두E", now());
+
+insert into team_hashtag (id, team_id, content, created_at)
+values (2,
+        1,
+        "보드게임", now());

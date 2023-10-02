68d7cb97bac544760671b8ce92d27d41663bbcfc
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-02 12:03:50 +0900

    feat : token data 관리 클래스 구현 (#18)

---
+++ module-core/src/main/java/com/everyTing/core/token/type/MemberTokens.java
@@ -0,0 +1,15 @@
+package com.everyTing.core.token.type;
+
+import lombok.Getter;
+
+@Getter
+public class MemberTokens {
+
+    private final String accessToken;
+    private final String refreshToken;
+
+    public MemberTokens(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}

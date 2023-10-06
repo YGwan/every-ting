27f5ce32f94f15f52ef32772b5e5a9662f88146e
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-06 13:35:26 +0900

    feat : cors filter 로직 구현 및 적용 (#26)

--- module-core/src/main/java/com/everyTing/core/config/FilterConfig.java
+++ module-core/src/main/java/com/everyTing/core/config/FilterConfig.java
@@ -1,9 +1,11 @@
 package com.everyTing.core.config;
 
+import com.everyTing.core.cors.CorsFilter;
 import com.everyTing.core.token.filter.TokenFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 
 import javax.servlet.Filter;
 
@@ -11,9 +13,11 @@ import javax.servlet.Filter;
 public class FilterConfig {
 
     private final TokenFilter tokenFilter;
+    private final CorsFilter corsFilter;
 
-    public FilterConfig(TokenFilter tokenFilter) {
+    public FilterConfig(TokenFilter tokenFilter, CorsFilter corsFilter) {
         this.tokenFilter = tokenFilter;
+        this.corsFilter = corsFilter;
     }
 
     @Bean
@@ -21,6 +25,15 @@ public class FilterConfig {
         var filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(tokenFilter);
         filterRegistrationBean.addUrlPatterns(TokenFilter.APPLY_URL_PATTERNS);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> addCorsFilter() {
+        var filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(corsFilter);
+        filterRegistrationBean.addUrlPatterns(CorsFilter.APPLY_URL_PATTERNS);
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return filterRegistrationBean;
     }
 }

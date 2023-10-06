2f8b7711db7cc2cf618c9c504206ebc8590b3eb5
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-06 23:16:23 +0900

    fix : core application 파일 형식 수정 및 적용 (#26)

--- module-member/src/main/java/com/everyTing/MemberApplication.java
+++ module-member/src/main/java/com/everyTing/MemberApplication.java
@@ -3,7 +3,7 @@ package com.everyTing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class MemberApplication {
 
     public static void main(String[] args) {

33d40d33b1c13fdc44718830d04379a40bf6f943
sol <sadlthf@icloud.com>
2023-10-08 02:07:30 +0900

    test : 팀 조회 api 테스트 (#21)

--- module-team/src/test/java/com/everyTing/team/adapter/out/persistence/TeamPersistenceAdapterTest.java
+++ module-team/src/test/java/com/everyTing/team/adapter/out/persistence/TeamPersistenceAdapterTest.java
@@ -12,15 +12,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.Mockito.mock;
 
 import com.everyTing.team.adapter.out.persistence.entity.TeamEntity;
-import com.everyTing.team.adapter.out.persistence.entity.TeamMemberEntity;
 import com.everyTing.team.adapter.out.persistence.repository.TeamEntityRepository;
 import com.everyTing.team.adapter.out.persistence.repository.TeamHashtagEntityJdbcRepository;
-import com.everyTing.team.adapter.out.persistence.repository.TeamMemberEntityRepository;
+import com.everyTing.team.domain.Team;
 import com.everyTing.team.utils.BaseTest;
 import com.everyTing.team.utils.TeamEntityFixture;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -39,16 +38,28 @@ class TeamPersistenceAdapterTest extends BaseTest {
     @Mock
     private TeamEntityRepository teamEntityRepository;
     @Mock
-    private TeamMemberEntityRepository teamMemberEntityRepository;
-    @Mock
     private TeamHashtagEntityJdbcRepository teamHashtagEntityJdbcRepository;
+
+    @DisplayName("팀 조회")
+    @Test
+    void findTeam() {
+        // given
+        given(teamEntityRepository.findById(any())).willReturn(Optional.of(teamEntity));
+
+        // when
+        Team createdTeam = sut.findTeam(1L);
+
+        // then
+        assertThat(createdTeam.getId()).isEqualTo(teamEntity.getId());
+    }
 
     @DisplayName("팀 생성")
     @Test
     void saveTeam() {
         // given
         given(teamEntityRepository.save(any())).willReturn(teamEntity);
-        willDoNothing().given(teamHashtagEntityJdbcRepository).saveAll(any());
+        willDoNothing().given(teamHashtagEntityJdbcRepository)
+                       .saveAll(any());
 
         // when
         Long createdTeamId = sut.saveTeam(

79cd667f2e158ea98da33022eb28faf449e31689
sol <sadlthf@icloud.com>
2023-10-09 01:57:56 +0900

    refactor : api 문서화를 위해 ControllerDocs 생성 (#23)

---
+++ module-team/src/main/java/com/everyTing/team/adapter/in/web/docs/TeamControllerDocs.java
@@ -0,0 +1,38 @@
+package com.everyTing.team.adapter.in.web.docs;
+
+import com.everyTing.core.dto.Response;
+import com.everyTing.team.adapter.in.web.request.TeamSaveRequest;
+import com.everyTing.team.domain.Team;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Tag(name = "팀 API")
+@RequestMapping("/api/v1/teams")
+public interface TeamControllerDocs {
+
+    @Operation(summary = "팀 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "조회 완료", useReturnTypeSchema = true),
+        @ApiResponse(responseCode = "404", description = "TEAM_006", content = @Content)})
+    @GetMapping("/{teamId}")
+    Response<Team> teamDetails(@PathVariable Long teamId);
+
+    @Operation(summary = "팀 생성")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "생성 완료, 생성된 팀의 id 반환", useReturnTypeSchema = true),
+        @ApiResponse(responseCode = "400", description = "TEAM_005", content = @Content),
+        @ApiResponse(responseCode = "400", description = "유효성 검사 실패", content = @Content)})
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Response<Long> teamSave(@RequestBody TeamSaveRequest request);
+}
--- module-team/src/main/java/com/everyTing/team/adapter/in/web/request/TeamSaveRequest.java
+++ module-team/src/main/java/com/everyTing/team/adapter/in/web/request/TeamSaveRequest.java
@@ -1,17 +1,25 @@
 package com.everyTing.team.adapter.in.web.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class TeamSaveRequest {
 
+    @Schema(description = "팀명, 3글자 이상 20글자 이하")
     private String name;
+
+    @Schema(description = "팀명 제한 수, 2명이상 6명 이하", defaultValue = "3")
     private Short memberLimit;
+
+    @Schema(description = "지역", defaultValue = "서울 남부")
     private String region;
+
+    @Schema(description = "해시태그, 1글자이상 10글자 이하", nullable = true)
     private List<String> hashtags;
 
-    public TeamSaveRequest() { 
+    public TeamSaveRequest() {
     }
 
     public TeamSaveRequest(String name, Short memberLimit, String region, List<String> hashtags) {

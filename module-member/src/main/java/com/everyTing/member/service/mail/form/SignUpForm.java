fa2de03fe45237755def3c884ec235196eeb2d67
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-11 02:35:37 +0900

    feat : 이메일 폼 데이터 생성 (#39)

---
+++ module-member/src/main/java/com/everyTing/member/service/mail/form/SignUpForm.java
@@ -0,0 +1,27 @@
+package com.everyTing.member.service.mail.form;
+
+public class SignUpForm implements MailForm {
+
+    private final String username;
+    private final String authCode;
+
+    public SignUpForm(String username, String authCode) {
+        this.username = username;
+        this.authCode = authCode;
+    }
+
+    @Override
+    public String title() {
+        return "EVERYTING 대학교 이메일 인증 메일입니다.";
+    }
+
+    @Override
+    public String body() {
+        return username + "님 \n"
+                + "저희 EVERYTING을 찾아주셔서 감사합니다.\n"
+                + "아래의 인증 코드를 입력해주세요.\n\n"
+                + "==========\n"
+                + authCode + "\n"
+                + "==========";
+    }
+}

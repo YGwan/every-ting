fba0cdfbb5bad03f20f268f63d53f1a12b997a1b
sol <sadlthf@icloud.com>
2023-10-10 18:57:43 +0900

    feat : 팀 코드로 팀 조회 api 레포지토리 작성 (#31)

--- module-team/src/main/java/com/everyTing/team/adapter/out/persistence/repository/TeamEntityRepository.java
+++ module-team/src/main/java/com/everyTing/team/adapter/out/persistence/repository/TeamEntityRepository.java
@@ -1,8 +1,11 @@
 package com.everyTing.team.adapter.out.persistence.repository;
 
 import com.everyTing.team.adapter.out.persistence.entity.TeamEntity;
+import com.everyTing.team.adapter.out.persistence.entity.data.Code;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamEntityRepository extends JpaRepository<TeamEntity, Long> {
 
+    Optional<TeamEntity> findByCode(Code code);
 }

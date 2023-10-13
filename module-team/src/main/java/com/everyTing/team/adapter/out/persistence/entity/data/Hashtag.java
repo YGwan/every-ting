35342b21007b11f5002690e6fed25a395e674564
sol <sadlthf@icloud.com>
2023-10-13 16:40:23 +0900

    feat : List validate 메소드 추가 (#43)

--- module-team/src/main/java/com/everyTing/team/adapter/out/persistence/entity/data/Hashtag.java
+++ module-team/src/main/java/com/everyTing/team/adapter/out/persistence/entity/data/Hashtag.java
@@ -1,10 +1,14 @@
 package com.everyTing.team.adapter.out.persistence.entity.data;
 
 import static com.everyTing.team.common.constraints.TeamConstraints.HASHTAG_MAX_LENGTH;
+import static com.everyTing.team.common.constraints.TeamConstraints.HASHTAG_MAX_LIMIT;
 import static com.everyTing.team.common.constraints.TeamConstraints.HASHTAG_MIN_LENGTH;
 import static com.everyTing.team.common.exception.errorCode.TeamErrorCode.TEAM_004;
+import static com.everyTing.team.common.exception.errorCode.TeamErrorCode.TEAM_007;
 
 import com.everyTing.core.exception.TingApplicationException;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
@@ -27,9 +31,23 @@ public class Hashtag {
         return new Hashtag(trimValue);
     }
 
+    public static List<Hashtag> from(List<String> values) {
+        validate(values);
+
+        return values.stream()
+                     .map(Hashtag::from)
+                     .collect(Collectors.toList());
+    }
+
     public static void validate(String value) {
         if (!(value.length() >= HASHTAG_MIN_LENGTH && value.length() <= HASHTAG_MAX_LENGTH)) {
             throw new TingApplicationException(TEAM_004);
+        }
+    }
+
+    public static void validate(List<String> values) {
+        if (values.size() > HASHTAG_MAX_LIMIT) {
+            throw new TingApplicationException(TEAM_007);
         }
     }
 

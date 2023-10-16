268782bd44f719b2c0c8eaf628391f2f1cc07e9d
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-16 22:39:16 +0900

    feat : 멤버 조회 response dto 구현 & 에러코드 추가 (#54)

---
+++ module-member/src/main/java/com/everyTing/member/dto/response/MyListResponse.java
@@ -0,0 +1,56 @@
+package com.everyTing.member.dto.response;
+
+import com.everyTing.core.domain.Gender;
+import com.everyTing.member.domain.Member;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class MyListResponse {
+
+    private Long id;
+
+    private String username;
+
+    private Gender gender;
+
+    private LocalDate birth;
+
+    private String universityEmail;
+
+    private String university;
+
+    private String major;
+
+    private String kakaoId;
+
+    private String profilePhoto;
+
+    public MyListResponse(Long id, String username, Gender gender, LocalDate birth, String universityEmail,
+                          String university, String major, String kakaoId, String profilePhoto) {
+        this.id = id;
+        this.username = username;
+        this.gender = gender;
+        this.birth = birth;
+        this.universityEmail = universityEmail;
+        this.university = university;
+        this.major = major;
+        this.kakaoId = kakaoId;
+        this.profilePhoto = profilePhoto;
+    }
+
+    public static MyListResponse from(Member member) {
+        return new MyListResponse(
+                member.getId(),
+                member.getUsername().getValue(),
+                member.getGender(),
+                member.getBirth(),
+                member.getUniversityEmail().getValue(),
+                member.getUniversity().getValue(),
+                member.getMajor().getValue(),
+                member.getKakaoId().getValue(),
+                member.getProfilePhoto()
+        );
+    }
+}

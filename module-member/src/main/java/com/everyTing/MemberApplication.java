7dcc06a99d98ad416ad87d9f957f6d5da719163a
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-18 13:32:45 +0900

    feat : core 모듈 의존성 타 모듈 처리 구조 변경 (#64)

--- module-member/src/main/java/com/everyTing/MemberApplication.java
+++ module-member/src/main/java/com/everyTing/MemberApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MemberApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application, application-core");
         SpringApplication.run(MemberApplication.class, args);
     }
 }

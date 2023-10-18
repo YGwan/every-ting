f8646696816db844baa6a2322fa04c55c4a82341
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-18 13:31:41 +0900

    feat : 비동기 처리 환경 설정 (#64)

---
+++ module-core/src/main/java/com/everyTing/core/config/AsyncConfig.java
@@ -0,0 +1,33 @@
+package com.everyTing.core.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Value("${pool.size.core}")
+    private int corePoolSize;
+
+    @Value("${pool.size.max}")
+    private int maxPoolSize;
+
+    @Value("${queue.capacity}")
+    private int queueCapacity;
+
+    @Bean
+    public Executor taskExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.initialize();
+        return executor;
+    }
+}

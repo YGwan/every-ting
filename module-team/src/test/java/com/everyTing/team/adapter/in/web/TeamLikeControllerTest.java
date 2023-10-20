e1e6718d51d01a450e2250c02d2069c970da2f37
sol <sadlthf@icloud.com>
2023-10-20 16:23:39 +0900

    test : 좋아요 조회 api 테스트 (#52)

--- module-team/src/test/java/com/everyTing/team/adapter/in/web/TeamLikeControllerTest.java
+++ module-team/src/test/java/com/everyTing/team/adapter/in/web/TeamLikeControllerTest.java
@@ -1,15 +1,25 @@
 package com.everyTing.team.adapter.in.web;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.everyTing.core.token.service.TokenService;
+import com.everyTing.team.adapter.out.persistence.entity.TeamEntity;
+import com.everyTing.team.adapter.out.persistence.entity.TeamLikeEntity;
+import com.everyTing.team.adapter.out.persistence.entity.TeamMemberEntity;
 import com.everyTing.team.application.port.in.TeamLikeUseCase;
+import com.everyTing.team.domain.TeamLikes;
+import com.everyTing.team.utils.BaseTest;
+import com.everyTing.team.utils.TeamEntityFixture;
+import com.everyTing.team.utils.TeamMemberEntityFixture;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +28,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(TeamLikeController.class)
-class TeamLikeControllerTest {
+class TeamLikeControllerTest extends BaseTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,6 +41,24 @@ class TeamLikeControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    TeamMemberEntity member1 = TeamMemberEntityFixture.get(1L);
+    TeamMemberEntity member2 = TeamMemberEntityFixture.get(2L);
+    TeamEntity fromTeam = TeamEntityFixture.get(1L);
+    TeamEntity toTeam = TeamEntityFixture.get(2L);
+    TeamLikeEntity teamLike1 = TeamLikeEntity.of(member1, fromTeam, toTeam);
+    TeamLikeEntity teamLike2 = TeamLikeEntity.of(member2, fromTeam, toTeam);
+
+    @DisplayName("팀 좋아요 조회 api 테스트")
+    @Test
+    void teamLikeList() throws Exception {
+        given(teamLikeUseCase.findTeamLike(any())).willReturn(TeamLikes.from(List.of(teamLike1, teamLike2)));
+
+        mockMvc.perform(get("/api/v1/teams/1/likes").param("toTeamId", "2"))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.data.fromTeamMemberIds.size()").value(2))
+               .andExpect(jsonPath("$.data").hasJsonPath());
+    }
 
     @DisplayName("팀 좋아요 api 테스트")
     @Test

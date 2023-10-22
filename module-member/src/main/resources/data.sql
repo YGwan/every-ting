a9e1762a893d0fe3e6ccd0c0f8e89db140f9d44e
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-23 00:42:32 +0900

    feat : 유저 더미데이터 생성 (#74)

---
+++ module-member/src/main/resources/data.sql
@@ -0,0 +1,109 @@
+-- 유저 더미 데이터
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (1,
+        "리나",
+        "FEMALE",
+        "1998",
+        "32190001@dankook.ac.kr",
+        "qwer1234!",
+        "단국대학교", "컴퓨터공학과",
+        "luckyStar123", "https://img.freepik.com/free-photo/portrait-of-young-woman-with-natural-make-up_23-2149084942.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697932800&semt=ais"
+        ,now(), now());
+
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (2,
+        "솔라",
+        "FEMALE",
+        "1999",
+        "32190002@dankook.ac.kr",
+        "qwer1234!",
+        "단국대학교", "컴퓨터공학과",
+        "dreamCatcher92", null
+        ,now(), now());
+
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (3,
+        "유나",
+        "FEMALE",
+        "2000",
+        "32190003@dankook.ac.kr",
+        "qwer1234!",
+        "단국대학교", "컴퓨터공학과",
+        "blazingFireworks", null
+           ,now(), now());
+
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (4,
+        "준호",
+        "MALE",
+        "1995",
+        "32180001@kyonggi.ac.kr",
+        "qwer1234!",
+        "경기대학교", "신소재공학과",
+        "midnightWhisperer", null
+           ,now(), now());
+
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (5,
+        "태민",
+        "MALE",
+        "1996",
+        "32180002@kyonggi.ac.kr",
+        "qwer1234!",
+        "경기대학교", "신소재공학과",
+        "cosmicExplorer", null
+           ,now(), now());
+
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (6,
+        "민준",
+        "MALE",
+        "1998",
+        "32180003@kyonggi.ac.kr",
+        "qwer1234!",
+        "경기대학교", "신소재공학과",
+        "secretAgentX", null
+           ,now(), now());
+
+
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (7,
+        "바람",
+        "MALE",
+        "1998",
+        "32170001@cau.ac.kr",
+        "qwer1234!",
+        "중앙대학교", "신소재공학과",
+        "moonlightD", null
+           ,now(), now());
+
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (8,
+        "고려",
+        "FEMALE",
+        "1998",
+        "32160001@korea.ac.kr",
+        "qwer1234!",
+        "고려대학교", "철학과",
+        "mystic12", null
+           ,now(), now());
+
+insert into member (id, username, gender, birth, university_email, password, university, major, kakao_id, profile_photo,
+                    created_at, updated_at)
+values (9,
+        "TEST",
+        "MALE",
+        "1998",
+        "test@snu.ac.kr",
+        "qwer1234!",
+        "서울대학교", "컴퓨터공학과",
+        "electrical12", "https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/283/f33f24fc1c6f69629e9c2ea9d3378d17_res.jpeg"
+           ,now(), now());

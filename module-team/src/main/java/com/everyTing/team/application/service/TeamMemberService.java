8df78d442bb6138bee1df398e6671685a67770c6
sol <sadlthf@icloud.com>
2023-10-23 19:45:27 +0900

    feat : 팀원 추방 api 서비스 작성 (#62)

--- module-team/src/main/java/com/everyTing/team/application/service/TeamMemberService.java
+++ module-team/src/main/java/com/everyTing/team/application/service/TeamMemberService.java
@@ -1,11 +1,17 @@
 package com.everyTing.team.application.service;
 
+import static com.everyTing.team.common.exception.errorCode.TeamErrorCode.TEAM_015;
+import static com.everyTing.team.common.exception.errorCode.TeamErrorCode.TEAM_026;
+
+import com.everyTing.core.exception.TingApplicationException;
 import com.everyTing.core.feign.dto.Member;
 import com.everyTing.team.application.port.in.TeamMemberUseCase;
 import com.everyTing.team.application.port.in.command.TeamMemberFindCommand;
+import com.everyTing.team.application.port.in.command.TeamMemberRemoveCommand;
 import com.everyTing.team.application.port.in.command.TeamMemberSaveCommand;
 import com.everyTing.team.application.port.out.MemberPort;
 import com.everyTing.team.application.port.out.TeamMemberPort;
+import com.everyTing.team.domain.TeamMember;
 import com.everyTing.team.domain.TeamMembers;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,5 +39,29 @@ public class TeamMemberService implements TeamMemberUseCase {
         final Member member = memberPort.getMemberById(command.getMemberId());
 
         return teamMemberPort.saveTeamMember(command.getTeamId(), member);
+    }
+
+    @Override
+    @Transactional
+    public void removeTeamMember(TeamMemberRemoveCommand command) {
+        final TeamMember teamLeader = teamMemberPort.findTeamLeader(command.getTeamId());
+
+        validateMemberIsTeamLeader(command.getMemberId(), teamLeader);
+        validateTeamMemberToBeRemovedIsNotTeamLeader(command.getTeamMemberId(), teamLeader);
+
+        teamMemberPort.removeTeamMember(command.getTeamId(), command.getTeamMemberId());
+    }
+
+    private void validateMemberIsTeamLeader(Long memberId, TeamMember teamLeader) {
+        if (teamLeader.getMemberId() != memberId) {
+            throw new TingApplicationException(TEAM_015);
+        }
+    }
+
+    private void validateTeamMemberToBeRemovedIsNotTeamLeader(
+        Long teamMemberId, TeamMember teamLeader) {
+        if (teamMemberId == teamLeader.getTeamMemberId()) {
+            throw new TingApplicationException(TEAM_026);
+        }
     }
 }

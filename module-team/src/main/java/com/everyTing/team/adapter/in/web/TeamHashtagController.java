b57e58fc90c83925b9d9ee7fba54db4ec167299e
sol <sadlthf@icloud.com>
2023-10-28 17:04:22 +0900

    feat : 해시태그 수정 api 컨트롤러 작성 (#88)

--- module-team/src/main/java/com/everyTing/team/adapter/in/web/TeamHashtagController.java
+++ module-team/src/main/java/com/everyTing/team/adapter/in/web/TeamHashtagController.java
@@ -1,13 +1,21 @@
 package com.everyTing.team.adapter.in.web;
 
 import com.everyTing.core.dto.Response;
+import com.everyTing.core.resolver.LoginMember;
+import com.everyTing.core.resolver.LoginMemberInfo;
 import com.everyTing.team.adapter.in.web.docs.TeamHashtagControllerDocs;
+import com.everyTing.team.adapter.in.web.request.TeamHashtagModifyRequest;
 import com.everyTing.team.application.port.in.TeamHashtagUseCase;
 import com.everyTing.team.application.port.in.command.TeamHashtagFindCommand;
+import com.everyTing.team.application.port.in.command.TeamHashtagModifyCommand;
 import com.everyTing.team.domain.TeamHashtags;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,8 +30,18 @@ public class TeamHashtagController implements TeamHashtagControllerDocs {
 
     @GetMapping
     public Response<TeamHashtags> hashtagList(@PathVariable Long teamId) {
-        TeamHashtagFindCommand command = TeamHashtagFindCommand.of(teamId);
-
+        final TeamHashtagFindCommand command = TeamHashtagFindCommand.of(teamId);
         return Response.success(hashtagUseCase.findHashtags(command));
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Response<Void> hashtagModify(@PathVariable Long teamId,
+        @RequestBody TeamHashtagModifyRequest request,
+        @LoginMember LoginMemberInfo loginMemberInfo) {
+        final TeamHashtagModifyCommand command = TeamHashtagModifyCommand.of(teamId,
+            request.getRemovedHashtagIds(), request.getNewHashtags(), loginMemberInfo.getId());
+        hashtagUseCase.modifyHashtags(command);
+        return Response.success();
     }
 }
--- module-team/src/main/java/com/everyTing/team/adapter/in/web/docs/TeamHashtagControllerDocs.java
+++ module-team/src/main/java/com/everyTing/team/adapter/in/web/docs/TeamHashtagControllerDocs.java
@@ -1,6 +1,8 @@
 package com.everyTing.team.adapter.in.web.docs;
 
 import com.everyTing.core.dto.Response;
+import com.everyTing.core.resolver.LoginMemberInfo;
+import com.everyTing.team.adapter.in.web.request.TeamHashtagModifyRequest;
 import com.everyTing.team.domain.TeamHashtags;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -16,4 +18,11 @@ public interface TeamHashtagControllerDocs {
         @ApiResponse(responseCode = "200", description = "조회 완료"),
         @ApiResponse(responseCode = "404", description = "TEAM_006", content = @Content)})
     Response<TeamHashtags> hashtagList(Long teamId);
+
+    @Operation(summary = "팀 해시태그 수정")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "수정 완료"),
+        @ApiResponse(responseCode = "403", description = "TEAM_015", content = @Content)})
+    Response<Void> hashtagModify(Long teamId, TeamHashtagModifyRequest request,
+        LoginMemberInfo loginMemberInfo);
 }

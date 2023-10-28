c57daabbc0f074371e843912d495629a9df543d7
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-29 01:40:51 +0900

    test : MemberFixture 추가 (#93)

---
+++ module-member/src/test/java/com/everyting/member/utils/MemberFixture.java
@@ -0,0 +1,34 @@
+package com.everyting.member.utils;
+
+import com.everyTing.core.domain.Gender;
+import com.everyTing.member.domain.Member;
+import com.everyTing.member.domain.data.*;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.now;
+
+public class MemberFixture {
+
+    public static final Username username = Username.from("Ygwan");
+    public static final Gender gender = Gender.MALE;
+    public static final BirthYear birth = BirthYear.from(1998);
+    public static final UniversityEmail universityEmail = UniversityEmail.from("32180000@dankook.ac.kr");
+    public static final Password password = Password.from("qwer1234!");
+    public static final University university = University.from("단국대학교");
+    public static final Major major = Major.from("컴퓨터공학과");
+    public static final KakaoId kakaoId = KakaoId.from("everyting");
+    public static final LocalDateTime createdAt = now();
+    public static final LocalDateTime updatedAt = now();
+
+    public static Member get(Long memberId) {
+        Member member = new Member(username, gender, birth, universityEmail, password,
+                university, major, kakaoId);
+        ReflectionTestUtils.setField(member, "id", memberId);
+        ReflectionTestUtils.setField(member, "createdAt", createdAt);
+        ReflectionTestUtils.setField(member, "updatedAt", updatedAt);
+
+        return member;
+    }
+}

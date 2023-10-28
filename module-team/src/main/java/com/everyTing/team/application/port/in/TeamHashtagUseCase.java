558f019818a3d8f22b1d39976c59144f5e18b328
sol <sadlthf@icloud.com>
2023-10-28 17:04:01 +0900

    feat : 해시태그 수정 비즈니스 로직 구현 (#88)
    member 가 팀장인지 확인 후에 removedHashtagIds 를 삭제, newHashtags 를 저장합니다.

--- module-team/src/main/java/com/everyTing/team/application/port/in/TeamHashtagUseCase.java
+++ module-team/src/main/java/com/everyTing/team/application/port/in/TeamHashtagUseCase.java
@@ -1,9 +1,12 @@
 package com.everyTing.team.application.port.in;
 
 import com.everyTing.team.application.port.in.command.TeamHashtagFindCommand;
+import com.everyTing.team.application.port.in.command.TeamHashtagModifyCommand;
 import com.everyTing.team.domain.TeamHashtags;
 
 public interface TeamHashtagUseCase {
 
     TeamHashtags findHashtags(TeamHashtagFindCommand command);
+
+    void modifyHashtags(TeamHashtagModifyCommand command);
 }

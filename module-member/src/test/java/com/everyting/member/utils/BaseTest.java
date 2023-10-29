3418aa276cb4a6dad454201e8aa445db7bef2e9a
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-29 22:49:52 +0900

    test : 멤버 회원가입 & 로그인 & 조회 테스트 로직 구현 (#93)

--- module-member/src/test/java/com/everyting/member/utils/BaseTest.java
+++ module-member/src/test/java/com/everyting/member/utils/BaseTest.java
@@ -1,7 +1,6 @@
 package com.everyting.member.utils;
 
 import com.everyTing.member.domain.data.constraints.MemberConstraints;
-import com.everyTing.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.util.ReflectionTestUtils;
 

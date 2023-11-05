fb3be364a74b7f85b6a1e8729fd23979e64b6b68
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-05 09:32:54 +0900

    feat : 생성된 사진 url(4개) 관련 embedded 클래스 구현 (#106)

---
+++ module-member/src/main/java/com/everyTing/member/domain/data/GeneratedImgUrls.java
@@ -0,0 +1,42 @@
+package com.everyTing.member.domain.data;
+
+import com.everyTing.core.exception.TingApplicationException;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+import static com.everyTing.member.domain.data.constraints.MemberConstraints.GENERATED_IMG_URLS_COUNT;
+import static com.everyTing.member.errorCode.MemberErrorCode.MEMBER_019;
+
+@Embeddable
+public class GeneratedImgUrls {
+
+    @NotNull
+    @Column(name = "generated_img_urls", length = 1024)
+    private String value;
+
+    protected GeneratedImgUrls() {
+    }
+
+    private GeneratedImgUrls(String value) {
+        this.value = value;
+    }
+
+    public static GeneratedImgUrls from(List<String> values) {
+        validate(values);
+        final String generateImgUrls = String.join(",", values);
+        return new GeneratedImgUrls(generateImgUrls);
+    }
+
+    public static void validate(List<String> values) {
+        if (values.size() != GENERATED_IMG_URLS_COUNT) {
+            throw new TingApplicationException(MEMBER_019);
+        }
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

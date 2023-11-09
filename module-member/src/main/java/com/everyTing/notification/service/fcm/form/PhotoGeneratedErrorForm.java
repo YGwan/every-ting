75689ce87cfdd705d11faf2d58c56aed9cb9ae69
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-09 21:59:44 +0900

    feat : 머신러닝 사진 생성 에러 메세지 폼 구현 (#116)

---
+++ module-member/src/main/java/com/everyTing/notification/service/fcm/form/PhotoGeneratedErrorForm.java
@@ -0,0 +1,15 @@
+package com.everyTing.notification.service.fcm.form;
+
+public class PhotoGeneratedErrorForm implements NotificationForm {
+
+    @Override
+    public String title() {
+        return "에러";
+    }
+
+    @Override
+    public String body() {
+        return "프로필 사진을 생성 과정에서 문제가 발생했습니다. \n" +
+                "재로그인 시에 다시 프로필을 설정하실 수 있습니다.";
+    }
+}

8b054fac0492f24cba323488816419aa6d572d30
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-09 14:43:47 +0900

    feat : 에러 폼 생성 서비스 구현 (#116)

---
+++ module-member/src/main/java/com/everyTing/notification/service/fcm/FcmFormCreator.java
@@ -0,0 +1,30 @@
+package com.everyTing.notification.service.fcm;
+
+import com.everyTing.notification.service.NotificationMetaService;
+import com.everyTing.notification.service.fcm.form.NotificationForm;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FcmFormCreator {
+
+    private final NotificationMetaService notificationMetaService;
+
+    public FcmFormCreator(NotificationMetaService notificationMetaService) {
+        this.notificationMetaService = notificationMetaService;
+    }
+
+    public Message makeMessage(Long memberId, NotificationForm form) {
+        final var pushToken = notificationMetaService.findPushTokenByMemberId(memberId);
+        return Message.builder()
+                .setToken(pushToken.getValue())
+                .setNotification(makeNotification(form.title(), form.body()))
+                .build();
+    }
+
+    private static Notification makeNotification(String title, String body) {
+        return Notification.builder().setTitle(title)
+                .setBody(body).build();
+    }
+}

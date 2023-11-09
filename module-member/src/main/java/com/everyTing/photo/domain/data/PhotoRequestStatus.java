a66d669e0fab06f265c514e36848d302122ac0f0
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-10 07:39:54 +0900

    feat : 프로필 사진 요청 관련 상태값 정의 (#120)

---
+++ module-member/src/main/java/com/everyTing/photo/domain/data/PhotoRequestStatus.java
@@ -0,0 +1,17 @@
+package com.everyTing.photo.domain.data;
+
+import lombok.Getter;
+
+@Getter
+public enum PhotoRequestStatus {
+    FAILED("실패"),
+    REQUESTING("요청중"),
+    COMPLETED("성공"),
+    NOT_FOUND("없음");
+
+    final String status;
+
+    PhotoRequestStatus(String status) {
+        this.status = status;
+    }
+}

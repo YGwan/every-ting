88c5979aef9d5fc3efb8d9abdf0761640b70834d
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-10 01:41:29 +0900

    feat : 생성된 이미지 테이블의 memberId에 index 적용 (#120)

--- module-member/src/main/java/com/everyTing/photo/domain/GeneratedPhoto.java
+++ module-member/src/main/java/com/everyTing/photo/domain/GeneratedPhoto.java
@@ -6,13 +6,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = @Index(name = "index_memberId", columnList = "memberId"))
 @Entity
 public class GeneratedPhoto {
 

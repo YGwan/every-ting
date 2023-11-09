0df850161ce4d0ad0d63a0e3f8028572cd657153
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-10 07:22:13 +0900

    feat : 프로필 사진 관련 정보 저장 로직 제거 (#120)

--- module-member/src/main/java/com/everyTing/photo/controller/PhotoController.java
+++ module-member/src/main/java/com/everyTing/photo/controller/PhotoController.java
@@ -4,9 +4,11 @@ import com.everyTing.core.dto.Response;
 import com.everyTing.core.resolver.LoginMember;
 import com.everyTing.core.resolver.LoginMemberInfo;
 import com.everyTing.member.service.MemberService;
-import com.everyTing.photo.service.GeneratedPhotoService;
 import com.everyTing.photo.service.S3Service;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/api/v1/photos")
@@ -22,8 +24,8 @@ public class PhotoController {
     }
 
     @PostMapping
-    public Response<Void> profilePhotoAdd(@LoginMember LoginMemberInfo memberInfo,
-                                          @RequestPart("profile_photo") MultipartFile multipartFile) {
+    public Response<Void> profilePhotoUploadAndUpdate(@LoginMember LoginMemberInfo memberInfo,
+                                                      @RequestPart("profile_photo") MultipartFile multipartFile) {
         final var memberId = memberInfo.getId();
         final var profilePhotoUrl = s3Service.uploadPhoto(memberId, multipartFile);
         memberService.modifyProfilePhoto(memberId, profilePhotoUrl);
--- module-member/src/main/java/com/everyTing/photo/repository/ProfilePhotoInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.everyTing.photo.repository;
-
-import com.everyTing.photo.domain.ProfilePhotoInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfilePhotoInfoRepository extends JpaRepository<ProfilePhotoInfo, Long> {
-}

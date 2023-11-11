9a467ad58794a8f0379cb7baf2c485f94069d894
sol <sadlthf@icloud.com>
2023-11-11 15:19:50 +0900

    docs : 내 팀 존재 여부 조회 api 스웨거 설정 (#89)

--- module-team/src/main/java/com/everyTing/team/adapter/in/web/docs/MyTeamControllerDocs.java
+++ module-team/src/main/java/com/everyTing/team/adapter/in/web/docs/MyTeamControllerDocs.java
@@ -7,6 +7,7 @@ import com.everyTing.team.adapter.out.persistence.entity.data.Role;
 import com.everyTing.team.common.exception.errorCode.details.MyTeamRemoveErrorCode;
 import com.everyTing.team.domain.TeamDates;
 import com.everyTing.team.domain.TeamRequests;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
@@ -22,6 +23,9 @@ public interface MyTeamControllerDocs {
 
     @Operation(summary = "내가 속한 팀 미팅 요청 현황 조회")
     Response<TeamRequests> myTeamRequestList(LoginMemberInfo loginMemberInfo);
+
+    @Hidden
+    Response<Boolean> myTeamExists(LoginMemberInfo loginMemberInfo);
 
     @Operation(summary = "속한 팀에서 나가기")
     @ApiErrorCode(values = MyTeamRemoveErrorCode.class)

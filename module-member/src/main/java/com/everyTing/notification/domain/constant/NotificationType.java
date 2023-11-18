5704f94e19793314008b3adf1883705e9b61fbca
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-18 13:52:21 +0900

    feat : notification type 구현 (#123)

---
+++ module-member/src/main/java/com/everyTing/notification/domain/constant/NotificationType.java
@@ -0,0 +1,10 @@
+package com.everyTing.notification.domain.constant;
+
+public enum NotificationType {
+    PROFILE_PHOTO_GENERATION_FAILED,
+    PROFILE_PHOTO_GENERATION_COMPLETED,
+    TEAM_DATE_MATCHED,
+    TEAM_DATE_REQUEST_REJECTED,
+    TEAM_DATE_REQUEST_SUCCESSED
+    ;
+}

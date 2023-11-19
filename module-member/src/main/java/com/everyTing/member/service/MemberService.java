0f2e40b7c4d386f657a9714e26b60919da635a20
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-19 22:17:57 +0900

    feat : 캐시 AOP 적용을 위한 service 코드 분리 (#125)

--- module-member/src/main/java/com/everyTing/member/service/MemberService.java
+++ module-member/src/main/java/com/everyTing/member/service/MemberService.java
@@ -2,19 +2,24 @@ package com.everyTing.member.service;
 
 import com.everyTing.core.exception.TingApplicationException;
 import com.everyTing.member.domain.Member;
-import com.everyTing.member.domain.data.*;
+import com.everyTing.member.domain.data.KakaoId;
+import com.everyTing.member.domain.data.Password;
+import com.everyTing.member.domain.data.ProfilePhoto;
+import com.everyTing.member.domain.data.Username;
 import com.everyTing.member.dto.response.MemberInfoResponse;
 import com.everyTing.member.dto.validatedDto.ValidatedPasswordResetRequest;
 import com.everyTing.member.dto.validatedDto.ValidatedSignInRequest;
 import com.everyTing.member.dto.validatedDto.ValidatedSignUpRequest;
 import com.everyTing.member.repository.MemberRepository;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.everyTing.member.errorCode.MemberErrorCode.*;
+import static com.everyTing.member.errorCode.MemberErrorCode.MEMBER_010;
+import static com.everyTing.member.errorCode.MemberErrorCode.MEMBER_016;
 
 @Transactional
 @Service
@@ -22,11 +27,13 @@ public class MemberService extends MemberServiceValidator {
 
     private final MemberRepository memberRepository;
     private final MemberDataDeleteService memberDataDeleteService;
+    private final MemberInquiryService memberInquiryService;
 
-    public MemberService(MemberRepository memberRepository, MemberDataDeleteService memberDataDeleteService) {
+    public MemberService(MemberRepository memberRepository, MemberDataDeleteService memberDataDeleteService, MemberInquiryService memberInquiryService) {
         super(memberRepository);
         this.memberRepository = memberRepository;
         this.memberDataDeleteService = memberDataDeleteService;
+        this.memberInquiryService = memberInquiryService;
     }
 
     public Long signUp(ValidatedSignUpRequest request) {
@@ -57,35 +64,40 @@ public class MemberService extends MemberServiceValidator {
 
     @Transactional(readOnly = true)
     public MemberInfoResponse findMemberInfo(Long memberId) {
-        final Member member = getMemberById(memberId);
+        final Member member = memberInquiryService.getMemberById(memberId);
         return MemberInfoResponse.from(member);
     }
 
+    @CachePut(cacheNames = "MemberCache", key = "#memberId", cacheManager = "memberCacheManager")
     public void modifyUsername(Long memberId, Username newUsername) {
         throwIfAlreadyExisted(newUsername);
-        final Member member = getMemberById(memberId);
+        final Member member = memberInquiryService.getMemberById(memberId);
         member.modifyUsername(newUsername);
     }
 
+    @CachePut(cacheNames = "MemberCache", key = "#memberId", cacheManager = "memberCacheManager")
     public void modifyKakaoId(Long memberId, KakaoId newValidateKakaoId) {
         throwIfAlreadyExisted(newValidateKakaoId);
-        final Member member = getMemberById(memberId);
+        final Member member = memberInquiryService.getMemberById(memberId);
         member.modifyKakaoId(newValidateKakaoId);
     }
 
+    @CachePut(cacheNames = "MemberCache", key = "#memberId", cacheManager = "memberCacheManager")
     public void modifyPassword(Long memberId, Password newPassword) {
-        final Member member = getMemberById(memberId);
+        final Member member = memberInquiryService.getMemberById(memberId);
         member.modifyPassword(newPassword);
     }
 
+    @CachePut(cacheNames = "MemberCache", key = "#memberId", cacheManager = "memberCacheManager")
     public void modifyProfilePhoto(Long memberId, String url) {
         final var profilePhoto = ProfilePhoto.from(url);
-        final Member member = getMemberById(memberId);
+        final Member member = memberInquiryService.getMemberById(memberId);
         member.modifyProfilePhoto(profilePhoto);
     }
 
+    @CachePut(cacheNames = "MemberCache", key = "#memberId", cacheManager = "memberCacheManager")
     public void resetPassword(ValidatedPasswordResetRequest validatedRequest) {
-        final Member member = getMemberByEmail(validatedRequest.getUniversityEmail());
+        final Member member = memberInquiryService.getMemberByEmail(validatedRequest.getUniversityEmail());
         member.modifyPassword(validatedRequest.getPassword());
     }
 
@@ -96,21 +108,9 @@ public class MemberService extends MemberServiceValidator {
     }
 
     public void throwIfNotValidatePassword(Long memberId, Password password) {
-        final Member member = getMemberById(memberId);
+        final Member member = memberInquiryService.getMemberById(memberId);
         if (!password.equals(member.getPassword())) {
             throw new TingApplicationException(MEMBER_016);
         }
-    }
-
-    private Member getMemberById(Long memberId) {
-        return memberRepository.findById(memberId).orElseThrow(() ->
-                new TingApplicationException(MEMBER_014)
-        );
-    }
-
-    private Member getMemberByEmail(UniversityEmail email) {
-        return memberRepository.findByUniversityEmail(email).orElseThrow(() ->
-                new TingApplicationException(MEMBER_014)
-        );
     }
 }

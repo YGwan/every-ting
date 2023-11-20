3c6b4544920b18c98494fc195b4ef8fa830f762e
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-21 02:53:14 +0900

    feat : 비밀번호 관련 암호화 처리시 발생하는 에러 코드 추가 (#129)

---
+++ module-member/src/main/java/com/everyTing/member/errorCode/MemberServerErrorCode.java
@@ -0,0 +1,24 @@
+package com.everyTing.member.errorCode;
+
+import com.everyTing.core.exception.errorCode.ServerErrorCode;
+
+public enum MemberServerErrorCode implements ServerErrorCode {
+    MSER_001("패스워드 암호화하는 과정에서 문제가 발생했습니다."),
+    ;
+
+    private String message;
+
+    MemberServerErrorCode(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getErrorCode() {
+        return this.name();
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}

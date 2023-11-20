e4760fbcd395641c9ac93797a09af7956a6e0e09
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-21 05:35:40 +0900

    feat : 비밀번호 수정 로직 구현 (캐시 처리 X) + 캐시 오류 해결(데이터 수정시 db 반영 문제 처리) (#129)

--- module-member/src/main/java/com/everyTing/member/service/MemberModificationService.java
+++ module-member/src/main/java/com/everyTing/member/service/MemberModificationService.java
@@ -5,22 +5,28 @@ import com.everyTing.member.domain.data.KakaoId;
 import com.everyTing.member.domain.data.Password;
 import com.everyTing.member.domain.data.ProfilePhoto;
 import com.everyTing.member.domain.data.Username;
+import com.everyTing.member.dto.request.PasswordModifyRequest;
+import com.everyTing.member.repository.MemberRepository;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberModificationService {
 
     private final MemberQueryService memberQueryService;
+    private final MemberRepository memberRepository;
 
-    public MemberModificationService(MemberQueryService memberQueryService) {
+    public MemberModificationService(MemberQueryService memberQueryService, MemberRepository memberRepository) {
         this.memberQueryService = memberQueryService;
+        this.memberRepository = memberRepository;
     }
 
     @CachePut(value = "member", key = "#memberId")
     public Member modifyUsername(Long memberId, Username newUsername) {
         final Member member = memberQueryService.findMemberById(memberId);
         member.modifyUsername(newUsername);
+        memberRepository.save(member);
         return member;
     }
 
@@ -28,13 +34,17 @@ public class MemberModificationService {
     public Member modifyKakaoId(Long memberId, KakaoId newValidateKakaoId) {
         final Member member = memberQueryService.findMemberById(memberId);
         member.modifyKakaoId(newValidateKakaoId);
+        memberRepository.save(member);
         return member;
     }
 
-    @CachePut(value = "member", key = "#memberId")
-    public Member modifyPassword(Long memberId, Password newPassword) {
+    @Transactional
+    public Member modifyPassword(Long memberId, PasswordModifyRequest request) {
         final Member member = memberQueryService.findMemberById(memberId);
-        member.modifyPassword(newPassword);
+
+        final Password encryptedPassword = Password.encryptedPassword(request.getPassword());
+        member.modifyPassword(encryptedPassword);
+        memberRepository.save(member);
         return member;
     }
 

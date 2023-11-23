c86f3b5fe736c71cfc23f09d85f2d34b0142f9b1
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-24 00:02:12 +0900

    feat : 멤버 데이터 암호화 변수 추가 (#129)

--- module-member/src/main/java/com/everyTing/member/utils/secureBundle/SecureBundle.java
+++ module-member/src/main/java/com/everyTing/member/utils/secureBundle/SecureBundle.java
@@ -8,14 +8,26 @@ public class SecureBundle {
 
     public static String PASSWORD_ENCRYPTION_ALGORITHM;
     public static int DEFAULT_SALT_LENGTH;
+    public static String MEMBER_ENCRYPTION_ALGORITHM;
+    public static String MEMBER_ENCRYPTION_KEY_ALGORITHM;
+    public static String MEMBER_ENCRYPTION_KEY;
 
     public SecureBundle(
-            @Value("${member.encryption.password.algorithm}")
+            @Value("${password.encryption.algorithm}")
             String passwordEncryptionAlgorithm,
-            @Value("${member.encryption.password.salt.length}")
-            int defaultSaltLength
+            @Value("${default.salt.length}")
+            int defaultSaltLength,
+            @Value("${member.encryption.algorithm}")
+            String memberDataEncryptionAlgorithm,
+            @Value("${member.encryption.key.algorithm}")
+            String memberEncryptionKeyAlgorithm,
+            @Value("${member.encryption.key.value}")
+            String memberEncryptionKey
     ) {
         PASSWORD_ENCRYPTION_ALGORITHM = passwordEncryptionAlgorithm;
         DEFAULT_SALT_LENGTH = defaultSaltLength;
+        MEMBER_ENCRYPTION_ALGORITHM = memberDataEncryptionAlgorithm;
+        MEMBER_ENCRYPTION_KEY = memberEncryptionKey;
+        MEMBER_ENCRYPTION_KEY_ALGORITHM = memberEncryptionKeyAlgorithm;
     }
 }

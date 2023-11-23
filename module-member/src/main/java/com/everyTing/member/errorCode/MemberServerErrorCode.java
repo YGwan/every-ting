eb0a0292c39e2d135461ca98dbff08477701cedc
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-24 00:57:36 +0900

    feat : 멤버 데이터 복호화 에러코드 추가 (#129)

--- module-member/src/main/java/com/everyTing/member/errorCode/MemberServerErrorCode.java
+++ module-member/src/main/java/com/everyTing/member/errorCode/MemberServerErrorCode.java
@@ -4,6 +4,8 @@ import com.everyTing.core.exception.errorCode.ServerErrorCode;
 
 public enum MemberServerErrorCode implements ServerErrorCode {
     MSER_001("패스워드 암호화하는 과정에서 문제가 발생했습니다."),
+    MSER_002("멤버 데이터 암호화하는 과정에서 문제가 발생했습니다."),
+    MSER_003("멤버 데이터 복호화 과정에서 문제가 발생했습니다."),
     ;
 
     private String message;

b7a82f1f109166a27ca77b2de8421e604d34d7cc
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-26 02:47:23 +0900

    feat : 토큰 관련 redis 테이블 생성 (#137)

--- module-core/src/main/java/com/everyTing/core/token/cache/MemberTokens.java
+++ module-core/src/main/java/com/everyTing/core/token/cache/MemberTokens.java
@@ -1,4 +1,24 @@
 package com.everyTing.core.token.cache;
 
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+
+import javax.persistence.Id;
+
+@Getter
+@RedisHash(value = "memberTokens", timeToLive = 259200)
 public class MemberTokens {
+
+    @Id
+    private final Long memberId;
+
+    private final String accessToken;
+
+    private final String refreshToken;
+
+    public MemberTokens(Long memberId, String accessToken, String refreshToken) {
+        this.memberId = memberId;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
 }

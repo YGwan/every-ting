9d05c64ab925151653e7a67309259ee879369fbf
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-27 17:20:08 +0900

    feat : token filter 경로 설정 (#142)

--- module-core/src/main/java/com/everyTing/core/token/filter/TokenFilter.java
+++ module-core/src/main/java/com/everyTing/core/token/filter/TokenFilter.java
@@ -4,6 +4,7 @@ import com.everyTing.core.token.exception.TokenException;
 import com.everyTing.core.token.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -13,19 +14,34 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
 public class TokenFilter extends OncePerRequestFilter {
 
     public static final String[] APPLY_URL_PATTERNS = new String[]{
-            "/api/v1/members/username/modify",
-            "/api/v1/members/password/modify",
-            "/api/v1/members/token/check",
+            "/api/v1/members/my/info",
             "/api/v1/members/password/check",
-            "/api/v1/members/my/info"
+            "/api/v1/members/username/modify",
+            "/api/v1/members/kakaoId/modify",
+            "/api/v1/members/password/modify",
+            "/api/v1/members",
+            "/api/v1/notifications/*",
+            "/api/v1/notification-metas/*",
+            "/api/v1/generated-photos/*",
+            "/api/v1/profile-photos/*"
     };
 
-    public static final String[] EXCLUDE_URL_PATTERNS = new String[]{};
+    public static final String[] EXCLUDE_URL_PATTERNS = new String[]{
+            "/api/v1/members/signUp",
+            "api/v1/members/info",
+            "/api/v1/members/signIn",
+            "/api/v1/members/username/check",
+            "/api/v1/members/kakaoId/check",
+            "/api/v1/members/password/reset",
+            "/api/v1/mail/*",
+            "/api/v1/token/*",
+    };
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TokenFilter.class);
 
@@ -48,5 +64,29 @@ public class TokenFilter extends OncePerRequestFilter {
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        String method = request.getMethod();
+
+        String path1 = "/api/v1/notifications";
+        String path2 = "/api/v1/generated-photos";
+        String path3 = "/api/v1/profile-photos/requests";
+
+        if (HttpMethod.POST.name().equals(method) && path1.equals(path)) {
+            return true;
+        }
+
+        if (HttpMethod.POST.name().equals(method) && path2.equals(path)) {
+            return true;
+        }
+
+        if (HttpMethod.PUT.name().equals(method) && path3.equals(path)) {
+            return true;
+        }
+
+        return Arrays.stream(EXCLUDE_URL_PATTERNS).anyMatch(path::startsWith);
     }
 }

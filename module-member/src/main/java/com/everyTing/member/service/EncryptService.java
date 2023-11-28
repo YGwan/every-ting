914cd80f702f752e2456be7ad65d00bcefa800cf
good0619@naver.com <32181771@dankook.ac.kr>
2023-11-29 03:29:35 +0900

    feat : digest 클래스 적용 (#144)

--- module-member/src/main/java/com/everyTing/member/service/EncryptService.java
+++ module-member/src/main/java/com/everyTing/member/service/EncryptService.java
@@ -1,6 +1,9 @@
 package com.everyTing.member.service;
 
+import com.everyTing.member.domain.data.Password;
+import com.everyTing.member.utils.RandomCodeUtils;
 import com.everyTing.member.utils.cipher.MemberDataCipher;
+import com.everyTing.member.utils.cipher.PasswordDigest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,23 +15,24 @@ import java.nio.charset.StandardCharsets;
 public class EncryptService {
 
     private final MemberDataCipher memberDataCipher;
+    private final PasswordDigest passwordDigest;
 
     public EncryptService(
             @Value("${password.encryption.algorithm}")
             String passwordEncryptionAlgorithm,
-            @Value("${default.salt.length}")
-            int defaultSaltLength,
             @Value("${member.encryption.algorithm}")
             String memberDataEncryptionAlgorithm,
             @Value("${member.encryption.key.algorithm}")
             String keyAlgorithm,
             @Value("${member.encryption.key.value}")
             String key) {
+
         this.memberDataCipher = new MemberDataCipher(
                 memberDataEncryptionAlgorithm,
                 new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), keyAlgorithm),
-                new IvParameterSpec(key.substring(0, 16).getBytes())
-        );
+                new IvParameterSpec(key.substring(0, 16).getBytes()));
+
+        this.passwordDigest = new PasswordDigest(passwordEncryptionAlgorithm);
     }
 
     public String encryptedMemberData(String plainText) {
@@ -37,5 +41,15 @@ public class EncryptService {
 
     public String decryptedMemberData(String cipherText) {
         return memberDataCipher.decrypt(cipherText);
+    }
+
+    public Password encryptedPassword(String password, String salt) {
+        Password.validate(password);
+        final String encryptedPassword = passwordDigest.encrypt(password, salt);
+        return new Password(encryptedPassword, salt);
+    }
+
+    public String issueSalt() {
+        return RandomCodeUtils.getSalt();
     }
 }

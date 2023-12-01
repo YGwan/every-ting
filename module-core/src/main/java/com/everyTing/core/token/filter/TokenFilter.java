720de1a0344f3ea3a516e60cd4bf4042686a537a
good0619@naver.com <32181771@dankook.ac.kr>
2023-12-01 19:57:23 +0900

    feat : filter 처리 구현 (#142)

--- module-core/src/main/java/com/everyTing/core/token/filter/TokenFilter.java
+++ module-core/src/main/java/com/everyTing/core/token/filter/TokenFilter.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -14,34 +15,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class TokenFilter extends OncePerRequestFilter {
 
-    public static final String[] APPLY_URL_PATTERNS = new String[]{
-            "/api/v1/members/my/info",
-            "/api/v1/members/password/check",
-            "/api/v1/members/username/modify",
-            "/api/v1/members/kakaoId/modify",
-            "/api/v1/members/password/modify",
-            "/api/v1/members",
-            "/api/v1/notifications/*",
-            "/api/v1/notification-metas/*",
-            "/api/v1/generated-photos/*",
-            "/api/v1/profile-photos/*"
-    };
+    public static final String[] APPLY_URL_PATTERNS = new String[]{"/*"};
 
-    public static final String[] EXCLUDE_URL_PATTERNS = new String[]{
-            "/api/v1/members/signUp",
-            "api/v1/members/info",
-            "/api/v1/members/signIn",
-            "/api/v1/members/username/check",
-            "/api/v1/members/kakaoId/check",
-            "/api/v1/members/password/reset",
-            "/api/v1/mail/*",
-            "/api/v1/token/*",
-    };
+    public static final List<HttpMethod> ALL_HTTP_METHODS = List.of(
+            HttpMethod.GET,
+            HttpMethod.HEAD,
+            HttpMethod.POST,
+            HttpMethod.PUT,
+            HttpMethod.DELETE
+    );
+
+    public static final Map<String, List<HttpMethod>> EXCLUDE_URL_PATTERNS_WITH_METHODS = new HashMap<>();
+
+    static {
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/members/signUp", List.of(HttpMethod.POST));
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/members/info", List.of(HttpMethod.GET));
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/members/signIn", List.of(HttpMethod.POST));
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/members/username/check", List.of(HttpMethod.GET));
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/members/kakaoId/check", List.of(HttpMethod.GET));
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/members/password/reset", List.of(HttpMethod.PUT));
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/mail/**", ALL_HTTP_METHODS);
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/token/**", ALL_HTTP_METHODS);
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/notifications", List.of(HttpMethod.POST));
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/generated-photos", List.of(HttpMethod.POST));
+        EXCLUDE_URL_PATTERNS_WITH_METHODS.put("/api/*/profile-photos/requests", List.of(HttpMethod.PUT));
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TokenFilter.class);
 
@@ -62,31 +67,16 @@ public class TokenFilter extends OncePerRequestFilter {
             response.getOutputStream().write(e.getMessage().getBytes());
             return;
         }
-
         filterChain.doFilter(request, response);
     }
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
-        String path = request.getRequestURI();
-        String method = request.getMethod();
+        final AntPathMatcher pathMatcher = new AntPathMatcher();
+        final var path = request.getRequestURI();
+        final var httpMethod = HttpMethod.valueOf(request.getMethod());
 
-        String path1 = "/api/v1/notifications";
-        String path2 = "/api/v1/generated-photos";
-        String path3 = "/api/v1/profile-photos/requests";
-
-        if (HttpMethod.POST.name().equals(method) && path1.equals(path)) {
-            return true;
-        }
-
-        if (HttpMethod.POST.name().equals(method) && path2.equals(path)) {
-            return true;
-        }
-
-        if (HttpMethod.PUT.name().equals(method) && path3.equals(path)) {
-            return true;
-        }
-
-        return Arrays.stream(EXCLUDE_URL_PATTERNS).anyMatch(path::startsWith);
+        return EXCLUDE_URL_PATTERNS_WITH_METHODS.keySet().stream().anyMatch(key ->
+                pathMatcher.match(key, path) && EXCLUDE_URL_PATTERNS_WITH_METHODS.get(key).contains(httpMethod));
     }
 }

124027a29ac3f9982c29ea9aae97ddf69ec09187
good0619@naver.com <32181771@dankook.ac.kr>
2023-12-16 20:47:02 +0900

    feat : add mail errorCode

--- module-common/src/main/java/com/everyTing/member/errorCode/MemberServerErrorCode.java
+++ module-common/src/main/java/com/everyTing/member/errorCode/MemberServerErrorCode.java
@@ -7,6 +7,7 @@ public enum MemberServerErrorCode implements ServerErrorCode {
     MSER_002("멤버 데이터를 암/복호화 할수 있는 cipher를 만드는데 문제가 발생했습니다."),
     MSER_003("멤버 데이터 암호화하는 과정에서 문제가 발생했습니다."),
     MSER_004("멤버 데이터 복호화 과정에서 문제가 발생했습니다."),
+    MSER_005("메일을 보내는 과정에서 문제가 발생했습니다."),
     ;
 
     private String message;

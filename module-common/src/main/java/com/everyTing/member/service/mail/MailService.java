0e224638f8ea830efe49b4e85cf94f00514fffad
good0619@naver.com <32181771@dankook.ac.kr>
2023-12-16 20:48:01 +0900

    feat : apply retry policy to mail service

--- module-common/src/main/java/com/everyTing/member/service/mail/MailService.java
+++ module-common/src/main/java/com/everyTing/member/service/mail/MailService.java
@@ -1,5 +1,6 @@
 package com.everyTing.member.service.mail;
 
+import com.everyTing.core.exception.TingServerException;
 import com.everyTing.member.dto.form.MailForm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
@@ -7,17 +8,26 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-@Async
+import static com.everyTing.member.errorCode.MemberServerErrorCode.MSER_005;
+
 @Service
 public class MailService {
 
     @Value("${mail.fromAddr}")
     private String fromAddr;
 
+    @Value("${mail.retry.count}")
+    private int retryCount;
+
     private final JavaMailSender mailSender;
 
     public MailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
+    }
+
+    @Async
+    public void sendMail(String userAddr, MailForm form) {
+        sendMail(userAddr, form, fromAddr);
     }
 
     public void sendMail(String userAddr, MailForm form, String fromAddr) {
@@ -26,14 +36,19 @@ public class MailService {
         mailMessage.setSubject(form.title());
         mailMessage.setText(form.body());
         mailMessage.setFrom(fromAddr);
-        sendMail(mailMessage);
+
+        sendMail(retryCount, mailMessage);
     }
 
-    public void sendMail(String userAddr, MailForm form) {
-        sendMail(userAddr, form, fromAddr);
-    }
+    public void sendMail(int retryCount, SimpleMailMessage mailMessage) {
+        if (retryCount < 0) {
+            throw new TingServerException(MSER_005);
+        }
 
-    public void sendMail(SimpleMailMessage mailMessage) {
-        mailSender.send(mailMessage);
+        try {
+            mailSender.send(mailMessage);
+        } catch (Exception e) {
+            sendMail(retryCount - 1, mailMessage);
+        }
     }
 }

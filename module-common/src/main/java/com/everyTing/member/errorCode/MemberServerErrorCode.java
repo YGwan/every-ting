b5b0edba7d0956b5c839613cf2c0205eae3fab1e
good0619@naver.com <32181771@dankook.ac.kr>
2024-01-31 23:57:51 +0900

    feat : add dummy data error code

--- module-common/src/main/java/com/everyTing/member/errorCode/MemberServerErrorCode.java
+++ module-common/src/main/java/com/everyTing/member/errorCode/MemberServerErrorCode.java
@@ -8,6 +8,7 @@ public enum MemberServerErrorCode implements ServerErrorCode {
     MSER_003("멤버 데이터 암호화하는 과정에서 문제가 발생했습니다."),
     MSER_004("멤버 데이터 복호화 과정에서 문제가 발생했습니다."),
     MSER_005("메일을 보내는 과정에서 문제가 발생했습니다."),
+    MSER_006("더미데이터 생성 과정에서 문제가 발생했습니다."),
     ;
 
     private String message;
